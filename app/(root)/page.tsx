de04a9da348209be50c7285e94bb39ca5ae69336
Arnab Paul <arnabict@outlook.com>
2025-04-03 14:18:01 +0200

    All Startups & Startup Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,22 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 55,
+      author: {
+        _id: 1,
+        name: "Adam",
+      },
+      _id: 1,
+      description: "This is a description.",
+      image: `https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg`,
+      category: "Manager",
+      title: "We Managers",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -19,6 +36,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((StartupCardType, number) => (
+              <StartupCard key={posts?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
