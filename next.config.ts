0d12dac64474a9b418ca1b4800c60e1c08e89b3f
Arnab Paul <arnabict@outlook.com>
2025-04-11 18:49:19 +0200

    Sanity Write Client && Revert to "next": "15.0.0-canary.183"

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // experimental: {
-  //   ppr: "incremental",
-  // },
-  // devIndicators: {
-  //   appIsrStatus: true,
-  //   buildActivity: true,
-  //   buildActivityPosition: "bottom-right",
-  // },
+  experimental: {
+    ppr: "incremental",
+    after: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
 };
 
 export default nextConfig;
